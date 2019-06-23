7fef2779b7fcf12e2675bb55fb304adb2676fed7
Bahman <rouhani74@gmail.com>
2019-06-23 19:46:30 +0430

    added detector
    not tests yet

---
+++ Detecrot.py
@@ -0,0 +1,45 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+
+
+class Detector:
+    """
+    build and save detector model
+    """
+    def __init__(self, image_size, conv_layers, kernel_sizes, pooling_sizes, drop_outs, dense_layer, batch_size=100):
+        model = keras.models.Sequential()
+        model.add(keras.layers.Input(shape=(-1, image_size[0], image_size[1]), batch_size=batch_size))
+
+        for layer, kernel, pool_size, drop_out in zip(conv_layers, kernel_sizes, pooling_sizes, drop_outs):
+            model.add(keras.layers.Conv2D(layer, kernel, padding='same'))
+            model.add(keras.layers.Activation('relu'))
+            model.add(keras.layers.MaxPool2D(pool_size=pool_size))
+            model.add(keras.layers.Dropout(drop_out))
+
+        model.add(keras.layers.Flatten())
+        model.add(keras.layers.Dense(dense_layer))
+        model.add(keras.layers.Activation('relu'))
+        model.add(keras.layers.Dropout(0.5))
+        model.add(keras.layers.Dense(2))
+        model.add(keras.layers.Activation('softmax'))
+        self.model = model
+        return self.model
+
+    def train(self, x_train, y_train, batch_size, epochs, x_test, y_test):
+        # initiate RMSprop optimizer
+        opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+        # Let's train the model using RMSprop
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer=opt,
+                           metrics=['accuracy'])
+
+        self.model.fit(x_train, y_train,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       validation_data=(x_test, y_test),
+                       shuffle=True)
+
